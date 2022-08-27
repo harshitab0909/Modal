74aacd0d5064e6ed5bea4bfe26f9a1e1e4db1840
harshitab0909 <harshita.bijlani0909@gmail.com>
2022-08-27 12:31:22 +0530

    Functionality

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnclose = document.querySelector('.close-modal');
+const btnopen = document.querySelectorAll('.show-modal');
+for (let i = 0; i < btnopen.length; i++)
+    btnopen[i].addEventListener('click', function () {
+        console.log("button");
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+btnclose.addEventListener('click',function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+overlay.addEventListener('click',function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
